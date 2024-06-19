61b4713f099d9420c1f6513b95e4ce124071d9c5
acastrauss <acastrauss@hotmail.com>
2024-06-19 12:05:46 +0200

    Added models for external api response

--- src/controllers/filesController.ts
+++ src/controllers/filesController.ts
@@ -1,14 +1,17 @@
 import axios from 'axios';
 import config from 'config';
 import { Request, Response } from 'express';
+import { FilesApiResponse } from '../models/filesApiResponse';
 
 const FILES_API: string = config.get("FILES_API");
 
 export async function getFiles(req: Request, res: Response){
     try {
         const resApi = await axios.get(FILES_API);
-        res.send(resApi.data);
+        const parsedResponse = resApi.data as FilesApiResponse;
+        res.send(parsedResponse);        
     } catch (error) {
-        throw new Error("Failed to get data from an API");
+        res.status(400);
+        res.send("Failed to get data from an API");
     }
 }
---
+++ src/models/filesApiResponse.ts
@@ -0,0 +1,5 @@
+import { File } from "./file";
+
+export interface FilesApiResponse{
+    files: File[];
+}
