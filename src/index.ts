84425beebdfb23c8a3dae21f22c7e3526cd7676d
acastrauss <acastrauss@hotmail.com>
2024-06-19 10:43:23 +0200

    added axios for requests, dotenv/config for env vars, made an initial controller for api/files which handles possible errors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
+import axios from 'axios';
 import express from 'express';
+
+// required for loading local .env file
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
+const FILES_API = process.env.FILES_API;
 
-app.get('/', (req, res) => {
-    res.send('Hello, TypeScript with Express!');
+if(!FILES_API){
+    throw new Error("API is not defined. Please add it as env var and restart");
+}
+
+app.get('/api/files', async (req, res) => {
+    try {
+        const res1 = await axios.get(FILES_API);
+        res.send(res1.data);
+    } catch (error) {
+        throw new Error("Failed to get data from an API");
+    }
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
