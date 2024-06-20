1665eb0ba3760bdad8029d2d2887016b07acdeef
acastrauss <acastrauss@hotmail.com>
2024-06-20 20:03:20 +0200

    Updated docker script

--- RunOnDocker.sh
+++ RunOnDocker.sh
@@ -1,2 +1,6 @@
+# Build API image
 docker build -t api-files .
+
+# Start container with previously built image
+# Map host's port 3000 to container's port 300
 docker run -d -p 3000:3000 --name api-files-prod api-files
